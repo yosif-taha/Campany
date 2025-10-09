a482036fba4b8a504d3bd4ae8c91ae1182306141
Yosif Taha <ywsfwhab314@gmail.com>
2025-10-09 21:22:15 +0300

    Department Module BLL

---
+++ Company.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Company.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+       IEnumerable<Department> GetAll();
+        Department? Get(int id);
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+    }
+}
--- Company.DAL/Data/Configurations/DepartmentConfiguration.cs
+++ Company.DAL/Data/Configurations/DepartmentConfiguration.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Company.DAL.Data.Configirations
 {
-    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
     {
         public void Configure(EntityTypeBuilder<Department> builder)
         {
