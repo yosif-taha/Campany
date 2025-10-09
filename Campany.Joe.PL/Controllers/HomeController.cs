b57acde3d708bee08519c12471413f46fa5181d2
Yosif Taha <ywsfwhab314@gmail.com>
2025-10-09 18:48:41 +0300

    Add project files.

---
+++ Campany.Joe.PL/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Campany.Joe.PL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Campany.Joe.PL.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
