a77669a67d9f1e5484eafecfb4efab13bb40d0cc
Yosif Taha <ywsfwhab314@gmail.com>
2025-10-14 19:59:14 +0300

    Implementation PartialView For (Employee and Department Views)

--- Campany.Joe.PL/Controllers/DepartmentController.cs
+++ Campany.Joe.PL/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Identity.Client;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Campany.Joe.PL.Controllers
 {
@@ -65,22 +66,36 @@ namespace Campany.Joe.PL.Controllers
         [HttpGet]
         public IActionResult Edit( int? id)
         {
-            //if (id == null) return BadRequest("Invalid Id");
-            //var department = _repository.Get(id.Value);
-            //if (department is null) return NotFound(new { StatusCode = 400, Message = $"Department With Id {id} Is Not Found" });
+            if (id == null) return BadRequest("Invalid Id");
+            var department = _repository.Get(id.Value);
+            if (department is null) return NotFound(new { StatusCode = 400, Message = $"Department With Id {id} Is Not Found" });
+            var departmentDto = new CreateDepartmentDtos() //Casting Model From "CreateDepartmentDtos" class To "Department" class 
+            {                                 //for save date of "model" in Our Database
+                Code = department.Code,
+                Name = department.Name,
+                CreateAt = department.CreateAt
 
-            return Details(id,"Edit");       
+            };
+            return View(departmentDto);     
         }
 
         [HttpPost] //Use when send data through form
         [ValidateAntiForgeryToken]//Use with any form, for prevent any external tool or app 
                                   //from accessing that form
-        public IActionResult Edit([FromRoute] int id,Department model)
+        public IActionResult Edit([FromRoute] int id,CreateDepartmentDtos model)
         {
             if (ModelState.IsValid) //ServerSide Validation To Props Inside CreateDepartmentDtos Class
             {
-                if(id!=model.Id) return BadRequest();//Error 400
-                var count = _repository.Update(model);
+                var department = new Department() //Casting Model From "CreateDepartmentDtos" class To "Department" class 
+                {                                 //for save date of "model" in Our Database
+                    Id=id,
+                    Code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+
+                };
+                //if (id!=model.Id) return BadRequest();//Error 400
+                var count = _repository.Update(department);
                 if (count > 0) //if saved data of "model" in our database, the "Add" fun will increase
                                //one, is returned to index 
                 {
