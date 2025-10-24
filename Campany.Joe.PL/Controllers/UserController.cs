4c5aa8ef0e3651c631da2e95ec8609f5a28c450d
Yosif Taha <ywsfwhab314@gmail.com>
2025-10-25 00:15:55 +0300

    implementation of authorization(Roles)

---
+++ Campany.Joe.PL/Controllers/UserController.cs
@@ -0,0 +1,146 @@
+﻿using Campany.Joe.PL.Dtos;
+using Campany.Joe.PL.Helpers;
+using Company.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Campany.Joe.PL.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(string? SearchInput)
+        {
+            IEnumerable<UsrToReturnDto> users;
+            if (string.IsNullOrEmpty(SearchInput))
+            {
+                users = _userManager.Users.Select(U => new UsrToReturnDto()
+                {
+                    Id = U.Id,
+                    UserName = U.UserName,
+                    FirstName = U.FirstName,
+                    LastName = U.LastName,
+                    Email = U.Email,
+                    Roles = _userManager.GetRolesAsync(U).Result
+                });
+            }
+            else
+            {
+                users = _userManager.Users.Select(U => new UsrToReturnDto()
+                {
+                    Id = U.Id,
+                    UserName = U.UserName,
+                    FirstName = U.FirstName,
+                    LastName = U.LastName,
+                    Email = U.Email,
+                    Roles = _userManager.GetRolesAsync(U).Result
+                }).Where(U => U.FirstName.ToLower().Contains(SearchInput.ToLower()));
+            }
+            return View(users);
+        }
+
+        public async Task<IActionResult> Details(string? id, string ViewName)
+        {
+            if (id == null) return BadRequest("Invalid Id");
+            var user = await _userManager.FindByIdAsync(id);
+            if (user is null) return NotFound(new { StatusCode = 400, Message = $"User With Id {id} Is Not Found" });
+            var Dto = new UsrToReturnDto()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Roles = _userManager.GetRolesAsync(user).Result
+            };
+            return View(ViewName, Dto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string? id)
+        {
+
+            if (id == null) return BadRequest("Invalid Id");
+            var user = await _userManager.FindByIdAsync(id);
+            if (user is null) return NotFound(new { StatusCode = 400, Message = $"User With Id {id} Is Not Found" });
+
+            var Dto = new UsrToReturnDto()
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email, 
+                Roles = _userManager.GetRolesAsync(user).Result
+            };
+
+
+
+            return View(Dto);
+        }
+
+        [HttpPost] //Use when send data through form
+        [ValidateAntiForgeryToken]//Use with any form, for prevent any external tool or app 
+                                  //from accessing that form
+        public async Task<IActionResult> Edit([FromRoute] string id, UsrToReturnDto model)
+        {
+            if (ModelState.IsValid) //ServerSide Validation To Props Inside CreateDepartmentDtos Class
+            {
+
+                if (id !=model.Id) return BadRequest("invalid id");
+               var user= await _userManager.FindByIdAsync(id);
+               if(user is null) return BadRequest("invalid operation !");
+               user.UserName = model.UserName;
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Email = model.Email;
+
+               var result= await _userManager.UpdateAsync(user);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(string? id)
+        {
+            //if (id == null) return BadRequest("Invalid Id");
+            //var department = _employeerepository.Get(id.Value);
+            //if (department is null) return NotFound(new { StatusCode = 400, Message = $"Department With Id {id} Is Not Found" });
+
+            return await Details(id, "Delete");
+        }
+        [HttpPost] //Use when send data through form
+        [ValidateAntiForgeryToken]//Use with any form, for prevent any external tool or app 
+                                  //from accessing that form
+        public async Task<IActionResult> Delete([FromRoute] string id, UsrToReturnDto model)
+        {
+            if (ModelState.IsValid) //ServerSide Validation To Props Inside CreateDepartmentDtos Class
+            {
+                if (id != model.Id) return BadRequest("invalid id");
+                var user = await _userManager.FindByIdAsync(id);
+                if (user is null) return BadRequest("invalid operation !");
+               
+
+                var result = await _userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+    }
+}
+
